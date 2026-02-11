80e74e5ee8462cc224b30d29fdfefb9d354f1f86
Rhyhanop <118273732+Rhyhanop@users.noreply.github.com>
2026-02-11 23:48:13 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from "./Navbar";
 import Hero from './components/Hero';
 import Menu from './components/Menu';
 import About from './components/About';
